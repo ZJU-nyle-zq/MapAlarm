40e19a244f6694f96d71b3e1c3716a8590770453
Bran <544524199@qq.com>
2014-12-16 20:05:37 +0800

    Add some files

---
+++ MapAlarm/Util/Util.h
@@ -0,0 +1,18 @@
+//
+//  Util.h
+//  MapAlarm
+//
+//  Created by 邓永辉 on 14/12/15.
+//  Copyright (c) 2014年 邓永辉. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define EARTH_RADIUS 6378.137
+
+@interface Util : NSObject
+
++ (double) getDistance:(double) latitude1 longtitude1: (double) longitude1 : (double) latitude2 : (double) longitude2;
+
++ (double) rad : (double) d;
+@end
